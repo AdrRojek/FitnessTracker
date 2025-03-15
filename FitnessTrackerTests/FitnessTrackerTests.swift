d66f21020e76e9488eb46a68e26b1f8dedbfdd1b
Adrian Rojek <72630248+AdrRojek@users.noreply.github.com>
2025-03-15 20:54:31 +0100

    Initial Commit

---
+++ FitnessTrackerTests/FitnessTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessTrackerTests.swift
+//  FitnessTrackerTests
+//
+//  Created by adrian on 15/03/2025.
+//
+
+import Testing
+@testable import FitnessTracker
+
+struct FitnessTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
